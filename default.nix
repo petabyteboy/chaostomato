da17b04dd34e511128ed37217430cb01f66841fd
Milan Pässler <me@pbb.lc>
2020-11-07 10:56:53 +0100

    build static binary

--- default.nix
+++ default.nix
@@ -1,12 +1,21 @@
 { sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs {}
-, src ? pkgs.lib.cleanSource ./. }:
+, src ? null }:
 
 let
-  naersk = pkgs.callPackage sources.naersk {};
+  overlay = self: super: {
+    naersk = self.callPackage sources.naersk (self.lib.optionalAttrs self.stdenv.targetPlatform.isStatic {
+      stdenv = self.buildPackages.stdenv;
+    });
+    chaostomato = self.naersk.buildPackage ({
+      src = if src != null then src else self.lib.cleanSource ./.;
+      nativeBuildInputs = with self.buildPackages; [ pkgconfig ];
+      buildInputs = with self; [ libressl ];
+      CARGO_BUILD_TARGET = self.stdenv.targetPlatform.config;
+    } // self.lib.optionalAttrs self.stdenv.targetPlatform.isStatic {
+      "CARGO_TARGET_${self.lib.toUpper self.stdenv.cc.suffixSalt}_LINKER" = "${self.buildPackages.llvmPackages_10.lld}/bin/lld";
+    });
+  };
 in
-  naersk.buildPackage {
-    inherit src;
-    nativeBuildInputs = with pkgs; [ pkgconfig ];
-    buildInputs = with pkgs; [ openssl ];
+  import sources.nixpkgs {
+    overlays = [ overlay ];
   }
